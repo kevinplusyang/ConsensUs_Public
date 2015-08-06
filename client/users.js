d6def5d956cd550284e4d93abeb42f7d79b7843c
QCmu <mu.qiaochu@gmail.com>
2015-08-06 13:30:03 -0400

    Construct default cells for new invitation

--- client/users.js
+++ client/users.js
@@ -53,7 +53,54 @@ Template.login.events({
     }
 });
 
+var updateWeight = function(proID,userID){
+      var weightArray = cellFindCol(1,proID,userID);
+      var sum = 0;
+      weightArray.forEach(function(cell){
+        sum =sum + Number(cell.data);
+      });
+      cellFindCol(2,proID,userID).forEach(function(cell){
+        Cells.update(cell._id,{$set: {data: cellFindOne(cell.row, 1,proID,userID).data/sum}});
+      });
+      // norWeightArray.forEach(function(cell){
+      //   cell.data=Number(cell.data)/sum;
+      // });
+}
+var updateTotal = function(proID,userID){
+      var totalArray = cellFindRow(-1,proID,userID);
 
+      totalArray.forEach(function(cell){
+        var sum = 0;
+        Col=cell.column;
+        var scoreCol = cellFindCol(Col,proID,userID);
+        scoreCol.forEach(function(cellInside){
+          if (Number(cellInside.row)>= 1) {
+            sum =sum + Number(cellFindOne(cellInside.row,2,proID,userID).data ) * Number(cellInside.data) ;
+          };
+        });
+        Cells.update(cell._id,{$set: {data: sum}});
+     });
+}
+var initialPageforInv = function(proID,userID){
+
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:-1,column:3,data:0,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:-1,column:4,data:0,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:0,column:3,data:'New York',createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:0,column:4,data:'Hawaii',createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:0,data:'Cost',createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:0,data:'Safety',createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:1,data:3,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:2,data:1,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:3,data:2,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:4,data:3,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:1,data:1,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:2,data:2,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:3,data:3,createdAt: new Date()});
+ Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:4,data:1,createdAt: new Date()});
+
+ updateWeight(proID,userID);
+ updateTotal(proID,userID);
+}
 
 Template.joinProject.events({
     'submit form': function(event){
@@ -65,6 +112,7 @@ Template.joinProject.events({
         $('[name=joinproject]').val('');
 
         Projects.update({_id:joinproject},{$push: {users: {userId:currentUser,username:name}}});
+        initialPageforInv(joinproject,currentUser);
 
     }
 });
