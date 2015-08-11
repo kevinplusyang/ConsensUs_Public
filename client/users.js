aaa1d3a989f874ced57d2a134253915f1cba3935
kevinplusyang <kevinplusyang@gmail.com>
2015-08-11 10:09:07 -0400

    Enable change password in profile
    Enable change password in profile

--- client/users.js
+++ client/users.js
@@ -168,3 +168,55 @@ Template.joinProject.events({
 });
 
 
+
+
+//Template.ResetPassword.helpers({
+//    resetPassword: function(){
+//        return Session.get('resetPassword');
+//    }
+//});
+//
+//Template.ResetPassword.events({
+//    'submit #resetPasswordForm': function(e, t) {
+//        e.preventDefault();
+//
+//        var resetPasswordForm = $(e.currentTarget),
+//            password = resetPasswordForm.find('#resetPasswordPassword').val(),
+//            passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
+//
+//        if (true) {
+//            Accounts.resetPassword(Session.get('resetPassword'), password, function(err) {
+//                if (err) {
+//                    console.log('We are sorry but something went wrong.');
+//                } else {
+//                    console.log('Your password has been changed. Welcome back!');
+//                    Session.set('resetPassword', null);
+//                }
+//            });
+//        }
+//        return false;
+//    }
+//});
+
+Template.changePassword.events({
+    'submit form': function(event){
+        event.preventDefault();
+
+        var oldPassword = $('[name=oldPassword]').val();
+        var newPassword = $('[name=newPassword]').val();
+
+
+
+        Accounts.changePassword(oldPassword,newPassword);
+
+        Accounts.forgot
+
+        Router.go('user');
+
+
+
+
+    }
+});
+
+
