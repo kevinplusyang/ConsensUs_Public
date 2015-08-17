294a45091111e3d11c55aa1af3737f48274504a4
kevinplusyang <kevinplusyang@gmail.com>
2015-08-17 15:18:01 -0400

    Add comments
    Add comments

--- client/chatRoom.js
+++ client/chatRoom.js
@@ -1,59 +1,64 @@
 /**
- * Created by MingYang on 8/4/15.
+ * Chatroom.js is the js file provide service for chatroom.html
  */
 
 
 Template.chatrooms.helpers({
+    //Return the chatItem by project ID for chat room to present
     'chatroom': function(PID){
         var currentProject = PID;
-
         return Chatrooms.find({projectId: currentProject},{sort:{createdAt:1}});
     }
-}),
+});
 
 
 
-    Template.chatItem.events({
-        'click .delete-chatItem': function(event){
-            event.preventDefault();
-            var documentID = this._id;
-            Chatrooms.remove({_id: documentID});
-        }
-
-    }),
-
-    Template.newChatItem.helpers({
-        'chatroom': function(){
-            return Chatrooms.find({projectId: currentProject},{sort:{createdAt:1}});
-        }
-    }),
-
-
-    Template.newChatItem.events({
-        'submit form': function(event){
-            event.preventDefault();
-
-            var chatContent = event.target.newItem.value;
-            //console.log(chatContent);
-            var currentProject = this.currentProjectt;
-            var currentUser = Meteor.userId();
-            //var alias = Meteor.users.find({_id: currentUser});
-            var names = Meteor.user().username;
+Template.chatItem.events({
+    //Try to delete an chat item by click delete
+    //This events is only for test use.
+    //We didn't present this function in the final version
+    'click .delete-chatItem': function(event){
+        event.preventDefault();
+        var documentID = this._id;
+        Chatrooms.remove({_id: documentID});
+    }
+});
 
 
 
-            console.log(names);
-            Chatrooms.insert({
-                content: chatContent,
-                createdAt: new Date(),
-                user: names,
-                tag1: "",
-                tag2: "",
-                projectId: currentProject,
-                createdBy: currentUser
-            });
-            $('[name = "newItem"]').val('');
+Template.newChatItem.helpers({
+    //Return the latest chat item.
+    'chatroom': function(){
+        return Chatrooms.find({projectId: currentProject},{sort:{createdAt:1}});
+    }
+});
 
-        }
 
-    })
+
+Template.newChatItem.events({
+    //Add a chat item in the chatroom database events
+
+    'submit form': function(event){
+        //Trigger action to add a chat item in database after press return
+        event.preventDefault();
+
+        var chatContent = event.target.newItem.value;
+        var currentProject = this.currentProjectt;
+        var currentUser = Meteor.userId();
+        var names = Meteor.user().username;
+
+        Chatrooms.insert({
+            //Insert chat item into the database
+            content: chatContent,
+            createdAt: new Date(),
+            user: names,
+            tag1: "",
+            tag2: "",
+            projectId: currentProject,
+            createdBy: currentUser
+        });
+
+        $('[name = "newItem"]').val('');
+        //Clear input item
+    }
+});
