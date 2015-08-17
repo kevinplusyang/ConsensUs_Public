9b10e5123ab3bdbc234b4bc2412ed6e2b12cebfe
QCmu <mu.qiaochu@gmail.com>
2015-08-17 15:11:51 -0400

    Add comments, clear files.

--- client/userinProject.js
+++ client/userinProject.js
@@ -1,29 +1,14 @@
 Template.userInProject.helpers({
    'userInProject': function(currentProjectt){
-      //console.log("cr:",currentProjectt);
+      
       var nowProject = Projects.findOne({_id: currentProjectt});
-      //console.log("find:", nowProject);
    	var one = nowProject.users;
-      //console.log(one);
+
    	for (var item in one)
    	{
    		one[item].pj=currentProjectt;
-   		//console.log(one[item])
    	}
-   	// one['pj'] = this._id;
-   	//console.log(currentProjectt);
     return one;
  }
- //   },
-
-
-	// 'getID': function(){
-	// 	var ID=this._id;
- //      console.log("this_id:",this._id);
-	// 	return ID;
-	// }
-
-
-
 
 });
--- client/users.js
+++ client/users.js
@@ -1,43 +1,3 @@
-// Accounts.ui.config({
-//     passwordSignupFields: "USERNAME_ONLY"
-//   });
-// var projAddUser=function(cuserID,userName,proID){
-
-// 	return Projects.update(proID,{$push: {users: {userID:cuserID,username:userName}}});
-// };
-
-
-
-//Template.ResetPassword.helpers({
-//    resetPassword: function(){
-//        return Session.get('resetPassword');
-//    }
-//});
-//
-//Template.ResetPassword.events({
-//    'submit #resetPasswordForm': function(e, t) {
-//        e.preventDefault();
-//
-//        var resetPasswordForm = $(e.currentTarget),
-//            password = resetPasswordForm.find('#resetPasswordPassword').val(),
-//            passwordConfirm = resetPasswordForm.find('#resetPasswordPasswordConfirm').val();
-//
-//        if (true) {
-//            Accounts.resetPassword(Session.get('resetPassword'), password, function(err) {
-//                if (err) {
-//                    console.log('We are sorry but something went wrong.');
-//                } else {
-//                    console.log('Your password has been changed. Welcome back!');
-//                    Session.set('resetPassword', null);
-//                }
-//            });
-//        }
-//        return false;
-//    }
-//});
-
-
-
 Template.changePassword.events({
     'submit form': function(event){
         event.preventDefault();
@@ -45,16 +5,9 @@ Template.changePassword.events({
         var oldPassword = $('[name=oldPassword]').val();
         var newPassword = $('[name=newPassword]').val();
 
-
-
         Accounts.changePassword(oldPassword,newPassword);
-
         Accounts.forgot
-
         Router.go('user');
-
-
-
 
     }
 });
@@ -107,6 +60,10 @@ Template.login.events({
     }
 });
 
+/**
+function: normalization of weight columns (column 1),
+update cells of column 2.
+**/
 var updateWeight = function(proID,userID){
     var weightArray = cellFindCol(1,proID,userID);
     var sum = 0;
@@ -114,11 +71,10 @@ var updateWeight = function(proID,userID){
         sum =sum + Number(cell.data);
     });
     cellFindCol(2,proID,userID).forEach(function(cell){
+
         Cells.update(cell._id,{$set: {data: cellFindOne(cell.row, 1,proID,userID).data/sum}});
     });
-    // norWeightArray.forEach(function(cell){
-    //   cell.data=Number(cell.data)/sum;
-    // });
+
 }
 var updateTotal = function(proID,userID){
     var totalArray = cellFindRow(-1,proID,userID);
@@ -137,20 +93,6 @@ var updateTotal = function(proID,userID){
 }
 var initialPageforInv = function(proID,userID){
 
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:-1,column:3,data:0,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:-1,column:4,data:0,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:0,column:3,data:'New York',createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:0,column:4,data:'Hawaii',createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:0,data:'Cost',createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:0,data:'Safety',createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:1,data:3,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:2,data:1,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:3,data:2,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:1,column:4,data:3,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:1,data:1,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:2,data:2,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:3,data:3,createdAt: new Date(),SDdata:0});
-    //Cells.insert({userID: userID, isReport : false ,projectID:proID,row:2,column:4,data:1,createdAt: new Date(),SDdata:0});
 
     var row = Projects.findOne({_id: proID}).rows;
     var column = Projects.findOne({_id: proID}).columns;
@@ -161,15 +103,7 @@ var initialPageforInv = function(proID,userID){
     console.log(column);
     console.log("=============");
 
-    //for(var m=1 ; m<=row ; m++){
-    //    for(var n=1 ; n<=column+2 ; n++){
-    //        console.log("rows");
-    //        console.log(m);
-    //        console.log("column");
-    //        console.log(n);
-    //        console.log("=============");
-    //    }
-    //}
+
 
     for(var k=3 ; k<=column+2 ; k++){
         var dataTemp = Cells.findOne({projectID: proID, isReport:true, row:-1, column:k}).data;
